90aeee083b8db40b6603943a8ef53a707175942b
Ahmad Al-Momani <81530210+AhmdAlMomani@users.noreply.github.com>
2021-04-22 16:45:06 +0300

    Add files via upload

---
+++ project-1.js
@@ -0,0 +1,27 @@
+const questions =[`A magnet would most likely attract which of the following?`,
+`In fancy hotels, it is traditional for what tantalizing treat to be left on your pillow?`,
+`In the United States, what is traditionally the proper way to address a judge?`,
+`The popular children's song "It's Raining, It's Pouring" mentions an "old man" doing what?`,
+`If someone asked to see your ID, what might you show them?`,
+`According to a common phrase, a person who takes chances is "going out on a" what?`,
+`Due to the geographical areas they represented, the opposing sides of the US Civil War were known by what names?`,
+`According to the old saying, "love of" what "is the root of all evil"?`,
+`A person who is not a banker and lends money at an extremely high interest rate is known as what?`,
+`A common piece of advice goes "be there or be" what?`
+];
+const options =[[`metal`,`plastic`,`wood`,`the wrong mane`],
+[`A pretzel`,`An apple`,`A mint`,`A photo of Wolf Blitzer`],
+[`Your honor`,`Your holiness`,`You da man!`,`Your eminence`],
+[`Snoring`,`Cooking`,`Laughing`,`Yelling at squirrels`],
+[`Your tonque`,`Your teeth`,`The door`,`Your passport`],
+[`Limb`,`Horse`,`Nude beach`,`Skateboard`],
+[`The Cities and the Farms`,`The Kool and the Gang`,`The North and the South`,`the Hills and the Valleys`],
+[`Food`,`Money`,`Clothing`,`Reality television`],
+[`Loan shark`,`Green snake`,`Paper tiger`,`Brother-in-law`],
+[`Square`,`Bare`,`All alone as usual`,`Aware`]
+];
+
+const correctAnsers =[`metal`,`A mint`,`Your honor`,`Snoring`,`Your passport`,
+`Limp`,`The North and the South`,`Money`,]
+
+
